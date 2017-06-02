c06aa3952de2bf61d5fd976c1ccf967be5c17d76
Pierre Dargham <pierre.dargham@globalis-ms.com>
2017-06-02 19:15:11 +0200

    version 0.1.0

---
+++ wpg-uploads-path.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:         WPG Uploads Path
+ * Plugin URI:          https://github.com/wp-globalis-tools/wpg-uploads-path
+ * Description:         Change WordPress uploads directory
+ * Author:              Pierre Dargham, Globalis Media Systems
+ * Author URI:          https://www.globalis-ms.com/
+ * License:             GPL2
+ *
+ * Version:             0.1.0
+ * Requires at least:   4.0.0
+ * Tested up to:        4.7.8
+ */
+
+namespace Globalis\WP\UploadsPath;
+
+if(defined('WP_UPLOADS_DIR') && defined('WP_UPLOADS_URL')) {
+	add_filter('pre_option_upload_path', function() { return WP_UPLOADS_DIR; });
+	add_filter('pre_option_upload_url_path', function() { return WP_UPLOADS_URL; });
+}
