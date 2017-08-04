c4bad00a69d81df467a63c79733bbeb2b586e46c
Pierre Dargham <pierre.dargham@globalis-ms.com>
2017-08-04 10:56:44 +0200

    Revert "version 0.2.0"
    This reverts commit 4006618a91e62b017f18b3363190134a9d17b73a.

--- wpg-uploads-path.php
+++ wpg-uploads-path.php
@@ -2,12 +2,12 @@
 /**
  * Plugin Name:         WPG Uploads Path
  * Plugin URI:          https://github.com/wp-globalis-tools/wpg-uploads-path
- * Description:         Change WordPress uploads directory and fix media filenames
+ * Description:         Change WordPress uploads directory
  * Author:              Pierre Dargham, Globalis Media Systems
  * Author URI:          https://www.globalis-ms.com/
  * License:             GPL2
  *
- * Version:             0.2.0
+ * Version:             0.3.0
  * Requires at least:   4.0.0
  * Tested up to:        4.7.8
  */
@@ -18,5 +18,3 @@ if(defined('WP_UPLOADS_DIR') && defined('WP_UPLOADS_URL')) {
 	add_filter('pre_option_upload_path', function() { return WP_UPLOADS_DIR; });
 	add_filter('pre_option_upload_url_path', function() { return WP_UPLOADS_URL; });
 }
-
-add_filter('sanitize_file_name', 'remove_accents', 10, 1);
